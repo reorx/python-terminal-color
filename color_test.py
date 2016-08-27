c930acf0f0de4899996506cae9e257f84196c2cc
reorx <novoreorx@gmail.com>
2016-08-27 17:48:33 +0800

    test together

--- color_test.py
+++ color_test.py
@@ -10,16 +10,15 @@ def test_named_colors():
         print getattr(color, i)(i) + color.red('compare')
 
 
-#def test_256_colors():
-#    for i in (range(10) + ['a', 'b', 'c', 'd', 'e', 'f']):
-#        hex = '#' + str(i) * 3
-#        print color.fg256(hex, hex)
-#
-#
-
 def test_decorations():
-    for i in ['bold', 'italic', 'underline']:
+    for i in ['bold', 'italic', 'underline', 'strike', 'blink']:
         print '#' + i
         for j in color_names:
             c = getattr(color, j)(j)
             print getattr(color, i)(c) + color.red('compare')
+
+
+def test_256_colors():
+    for i in (range(10) + ['a', 'b', 'c', 'd', 'e', 'f']):
+        hex = str(i) * 3
+        print color.fg256(hex, hex)
