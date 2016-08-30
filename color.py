55641740961d4cf4b9792b1a6efa74baae8ec600
reorx <novoreorx@gmail.com>
2016-08-30 15:08:35 +0800

    use different END for fg, bg, hl, so that they won't reset each other

--- color.py
+++ color.py
@@ -145,30 +145,32 @@ def make_color(start, end):
 
 
 # According to https://en.wikipedia.org/wiki/ANSI_escape_code#graphics ,
-# 39 is reset for foreground, 49 is reset for background, for convenience,
-# we can just use 0 to reset all
+# 39 is reset for foreground, 49 is reset for background, 0 is reset for all
+# we can use 0 for convenience, but it will make color combination behaves weird.
 END = esc(0)
 
-black = make_color(esc(30), END)
-red = make_color(esc(31), END)
-green = make_color(esc(32), END)
-yellow = make_color(esc(33), END)
-blue = make_color(esc(34), END)
-magenta = make_color(esc(35), END)
-cyan = make_color(esc(36), END)
-white = make_color(esc(37), END)
+FG_END = esc(39)
+black = make_color(esc(30), FG_END)
+red = make_color(esc(31), FG_END)
+green = make_color(esc(32), FG_END)
+yellow = make_color(esc(33), FG_END)
+blue = make_color(esc(34), FG_END)
+magenta = make_color(esc(35), FG_END)
+cyan = make_color(esc(36), FG_END)
+white = make_color(esc(37), FG_END)
 
-black_bg = make_color(esc(40), END)
-red_bg = make_color(esc(41), END)
-green_bg = make_color(esc(42), END)
-yellow_bg = make_color(esc(43), END)
-blue_bg = make_color(esc(44), END)
-magenta_bg = make_color(esc(45), END)
-cyan_bg = make_color(esc(46), END)
-white_bg = make_color(esc(47), END)
+BG_END = esc(49)
+black_bg = make_color(esc(40), BG_END)
+red_bg = make_color(esc(41), BG_END)
+green_bg = make_color(esc(42), BG_END)
+yellow_bg = make_color(esc(43), BG_END)
+blue_bg = make_color(esc(44), BG_END)
+magenta_bg = make_color(esc(45), BG_END)
+cyan_bg = make_color(esc(46), BG_END)
+white_bg = make_color(esc(47), BG_END)
 
-#HL_END = esc(22, 27, 39)
-HL_END = esc(22, 27, 0)
+HL_END = esc(22, 27, 39)
+#HL_END = esc(22, 27, 0)
 
 black_hl = make_color(esc(1, 30, 7), HL_END)
 red_hl = make_color(esc(1, 31, 7), HL_END)
@@ -179,7 +181,6 @@ magenta_hl = make_color(esc(1, 35, 7), HL_END)
 cyan_hl = make_color(esc(1, 36, 7), HL_END)
 white_hl = make_color(esc(1, 37, 7), HL_END)
 
-# From http://askubuntu.com/a/528938/136672 , end = 0 is OK
 bold = make_color(esc(1), esc(22))
 italic = make_color(esc(3), esc(23))
 underline = make_color(esc(4), esc(24))
---
+++ test/combination_test.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import color
+
+
+def test_combination():
+    print
+    tpl = color.green('the quick {} jump over the {} dog')
+    f = tpl.format(
+        color.yellow('brown fox'),
+        color.red_bg('lazy'),
+    )
+    print repr(f)
+    print f
+
+    tpl = color.green_bg('the quick {} jump over the {} dog')
+    f = tpl.format(
+        color.yellow('brown fox'),
+        color.red_bg('lazy'),
+    )
+    print repr(f)
+    print f
